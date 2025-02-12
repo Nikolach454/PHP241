361da84306e9f9097557515c1debbc89b19ca875
Nikolach454 <kolamiloserdov326@gmail.com>
2025-02-12 17:27:54 +0300

    Сделал невероятное

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Милосердов Николай 241-321 Лабораторня 1</title>
 </head>
-<body>
-    <p><?php echo date("M/Y"); ?></p>
+<body style="background-color: cadetblue;">
+    <header style="display: flex; justify-content: space-between;">
+        <img src="./1000011505-photoaidcom-invert.jpg" alt="Лучший вуз в Политехе" style="width: 30%; height: 30%;">
+        <h1>2.1.Домашняя работа: Hello, World! Время выполнения - 3 часа</h1>
+    </header>
+    <main>
+        <p><?php echo "Hellow, world!!!!!!!!!" ?></p>
+    </main>
+    <footer>
+        <p>задание для самостоятельно работы (без описания):</p>
+        <p><?php echo date("M/Y"); ?></p>
+    </footer>
 </body>
 </html>
